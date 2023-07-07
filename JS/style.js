3f9c1e973d52592dcc5f3116fa0b9a0766a015c6
marxus-Tz <maxiusemmanueli@gmail.com>
2023-07-07 23:36:11 +0300

    add hero-section

--- JS/style.js
+++ JS/style.js
@@ -1,9 +1,14 @@
+let menuBtn = document.querySelector('#menu-btn');
+let nav = document.querySelector('.header .flex .navbar');
 
-let menuBtn= document.querySelector('#menu-btn');
-let nav= document.querySelector('.header .flex .navbar');
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle('fa-times');
+  nav.classList.toggle('active');
 
-menuBtn.oncllck=()=>{
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-
+  if (nav.classList.contains('active')) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+  }
+  
+};
