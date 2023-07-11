f44542aa76122ea488316f7203402a935d3410c6
marxus-Tz <maxiusemmanueli@gmail.com>
2023-07-11 21:21:12 +0300

    added footer by Marwa Emmanuel

--- JS/style.js
+++ JS/style.js
@@ -13,15 +13,12 @@ menuBtn.onclick = () => {
   
 };
 
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop:true,
-  centreSlide:'true',
-  fade:'true',
-  gragCursor:'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
